00efd5178f95d23f99a9d9774fbff9eb97d80dfa
shemar boyd <shemarlion@gmail.com>
2023-09-11 17:34:07 -0400

    2

---
+++ script.js
@@ -0,0 +1,32 @@
+function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+});
+
+const sections = document.querySelectorAll('.fade-in-section');
+
+sections.forEach((section) => {
+    observer.observe(section);
+});
+
+// Function to reset the fade-in effect when scrolled back to the top
+function resetFadeInEffect() {
+    sections.forEach((section) => {
+        if (!section.classList.contains('show')) {
+            observer.observe(section);
+        }
+    });
+}
+
+// Event listener to trigger reset when user scrolls
+window.addEventListener('scroll', resetFadeInEffect);
