cd2d1b2f0193e0b45ae4a1e227df8c87220a39c0
shemar boyd <shemarlion@gmail.com>
2023-09-21 20:16:13 -0400

    0

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ function resetFadeInEffect() {
 window.addEventListener('scroll', resetFadeInEffect);
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const buyNowButton = document.getElementById("buyNowButton");
+    const cartPopup = document.getElementById("cartPopup");
+    const closeCartPopupButton = document.getElementById("closeCartPopup");
+
+    buyNowButton.addEventListener("click", function() {
+        cartPopup.classList.remove("hidden");
+
+        // You can dynamically change the image source here
+        const productImage = document.createElement("img");
+        productImage.src = "imgs/IMG_7247 (1).jpg";
+        productImage.alt = "Product Image";
+        productImage.classList.add("w-32", "h-32", "mb-4", "mx-auto");
+        
+        const popupContent = document.querySelector("#cartPopup .bg-white");
+        popupContent.insertBefore(productImage, popupContent.firstChild);
+    });
+
+    closeCartPopupButton.addEventListener("click", function() {
+        cartPopup.classList.add("hidden");
+
+        // Remove the added image when closing the popup
+        const productImage = document.querySelector("#cartPopup img");
+        if (productImage) {
+            productImage.remove();
+        }
+    });
+});
